0890c7222125ada92d7429b6bd9d8e980914720c
CreakyCoss <1085892854@qq.com>
2018-05-24 00:15:16 +0800

    修改编码规范

--- manage.py
+++ manage.py
@@ -19,6 +19,8 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
                 Permission=Permission, Post=Post, Comment=Comment)
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
