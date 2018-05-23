fe3706b09ac2cacdc77a7820fc012880fd292573
CreakyCoss <1085892854@qq.com>
2018-05-24 00:17:47 +0800

    添加flask_admin模块

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
 from flask_pagedown import PageDown
+from flask_admin import Admin
+from flask_babelex import Babel
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -15,6 +17,9 @@ pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+admin = Admin(name='后台')
+babel = Babel()
 
 
 def create_app(config_name):
@@ -29,6 +34,9 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
+    admin.init_app(app)
+    babel.init_app(app)
+
     if app.config['SSL_REDIRECT']:
         from flask_sslify import SSLify
         sslify = SSLify(app)
@@ -38,6 +46,10 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    # 自定义后台管理
+    from .backstage import backstage as backstage_blueprint
+    app.register_blueprint(backstage_blueprint)
 
     from .api_1_0 import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
